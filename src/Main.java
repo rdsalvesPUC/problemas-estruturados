f81aa44e692b2394f99172b65c3e3c7fed1dfc19
rodrigo.salves <s.rodrigo5@pucpr.edu.br>
2026-03-23 20:13:41 -0300

    classe pronta! falta testar

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-        MyArrayList<Integer> list1 = new MyArrayList<>(GLOBAL_CAPACITY);
-        MyArrayList<Integer> list2 = new MyArrayList<>();
-
-        list1.add(10);
-
-//        for (int i = 0; i < 2; i++) {
-//            System.out.println(list2.data[i]);
-//        }
-        list1.add(2);
-
-        System.out.println(list1.capacity);
-        System.out.println(list2.capacity);
-        System.out.println(list1.size);
-        System.out.println(list2.size);
-        System.out.println(list1.get(0));
-
     }
 }
--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,14 +1,17 @@
 public class MyArrayList<T> {
-    public T[] data;
-    public int capacity;
-    public int size;
+    private T[] data;
+    private int capacity;
+    private int size;
 
     // Constructors
     public MyArrayList() {
         this(10);
     }
-
     public MyArrayList(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("A capacidade deve ser maior que 0");
+        }
+
         this.data = (T[]) new Object[capacity];
         this.capacity = capacity;
         this.size = 0;
@@ -17,51 +20,65 @@ public class MyArrayList<T> {
     // Methods
     public void add(T valor){
         if (isArrayFull()) {
-            System.out.println("Array cheio");
-            return;
+            increaseArray();
         }
 
         data[size] = valor;
         size++;
     }
     public void add(int index, T valor){
-        if (isIndexValidForAdd(index)) {
-            if (isArrayFull()) {
-                increaseArray();
-            }
-
-            for (int i = this.size; i >= (index + 1) ; i--) {
-                data[i] = data[i-1];
-            }
-
-            data[index] = valor;
-            size++;
-        } else {
-            System.out.println("Índice inválido");
+        if (!isIndexValidForAdd(index)) {
+            throw new IndexOutOfBoundsException("Índice inválido");
         }
+
+        if (isArrayFull()) {
+            increaseArray();
+        }
+
+        for (int i = this.size; i >= (index + 1) ; i--) {
+            data[i] = data[i-1];
+        }
+
+        data[index] = valor;
+        size++;
     }
     public void remove(T valor){
         int index = indexOf(valor);
 
         if (index == -1) {
-            System.out.println("Valor inexistente no array");
+            throw new IndexOutOfBoundsException("Valor inexistente no array");
         }
 
-        data[index] = null;
-        size--;
+        remove(index);
     }
     public void remove(int index){
+        if (!isIndexValid(index)) {
+            throw new IndexOutOfBoundsException("Índice inválido");
+        }
 
+        for (int i = index; i < (this.size - 1) ; i++) {
+            data[i] = data[i + 1];
+        }
+
+        data[size - 1] = null;
+        size--;
     }
     public void set(int index, T valor){
+        if (!isIndexValid(index)) {
+            System.out.println("Índice inválido");
+        }
+
         data[index] = valor;
-        size++;
     }
     public T get(int index){
+        if (!isIndexValid(index)) {
+            throw new IndexOutOfBoundsException("Índice inexistente");
+        }
+
         return data[index];
     }
-    public void contains(T valor){
-
+    public boolean contains(T valor){
+        return indexOf(valor) != -1;
     }
     public int indexOf(T valor) {
         for (int i = 0; i < this.size; i++) {
@@ -73,25 +90,33 @@ public class MyArrayList<T> {
         }
         return -1;
     }
-    public void toArray(){
+    public T[] toArray(){
+        int customCapacity = this.size;
+        T[] array = (T[]) new Object[customCapacity];
 
+        for (int i = 0; i < customCapacity; i++) {
+            array[i] = data[i];
+        }
+
+        return array;
     }
 
     // Support Methods
     public boolean isArrayFull(){
         return (size == capacity);
     }
-
     public boolean isIndexValidForAdd(int index) {
         return (index >= 0 && index <= this.size);
     }
-
     public boolean isIndexValid(int index) {
         return (index >= 0 && index < this.size);
     }
-
     public void increaseArray() {
         int newCapacity = capacity + (capacity/2);
+
+        if (newCapacity <= this.capacity) {
+            newCapacity = capacity * 2;
+        }
 
         T[] newArray = (T[]) new Object[newCapacity];
 
